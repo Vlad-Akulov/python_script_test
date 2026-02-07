ba3032892b26c2defeb680a832b8bf91919c3be1
Volodymyr Akulov <onc736@usask.ca>
2026-02-07 01:01:47 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>Flask Test</title>
+      </head>
+      <body>
+        <h1>Hello from Flask</h1>
+        <p>This is raw HTML.</p>
+      </body>
+    </html>
+    """
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 0))  # 0 = random available port
+    app.run(host="0.0.0.0", port=port)
