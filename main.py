ae6510ee6e02208ab704e50f50c5a42622224de5
Volodymyr Akulov <onc736@usask.ca>
2026-02-07 02:06:52 -0600

    changed ouptut port ot be random

--- main.py
+++ main.py
@@ -19,5 +19,5 @@ def index():
     """
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 0))  # 0 = random available port
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
